074daff103bbc70a01ff8137d3cfe2e842605610
abdo elsaeed <98925457+abdoelsaeed@users.noreply.github.com>
2026-01-24 04:49:15 +0200

    Enhance main.ts with cookie parser and trust proxy
    Added cookie parser middleware and set trust proxy.

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -1,30 +1,38 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-
-import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     rawBody: true,
     bodyParser: true,
   });
+
   app.use(cookieParser());
   app.setGlobalPrefix('api/v1');
+
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL, // لازم يكون دومين vercel
     credentials: true,
   });
-
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: true, // منع الحقول غير المعرفة
+      forbidNonWhitelisted: true,
     }),
   );
+
+  // ✅ السطر المهم جدًا جدًا
+  const server = app.getHttpAdapter().getInstance();
+  server.set('trust proxy', 1);
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
